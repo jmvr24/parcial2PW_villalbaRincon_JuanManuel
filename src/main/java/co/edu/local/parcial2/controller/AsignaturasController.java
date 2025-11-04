8d93eef29935795ad3cd26e55a72b6976eece0bd
Juan Manuel Villalba Rincon <juanvillalbarincon@gmail.com>
2025-11-04 07:33:14 -0500

    creacion de los controladores

---
+++ src/main/java/co/edu/local/parcial2/controller/AsignaturasController.java
@@ -0,0 +1,124 @@
+package co.edu.local.parcial2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.edu.local.parcial2.dto.AsignaturaDTO;
+import co.edu.local.parcial2.model.Asignatura;
+import co.edu.local.parcial2.services.AsignaturaService;
+import co.edu.local.parcial2.services.UsuarioService;
+@Controller
+@RequestMapping("/rector")
+public class AsignaturasController {
+	
+	@Autowired
+	private AsignaturaService asignaturaService;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping("/usuarios/listar")
+	public String mostrarListaUsuarios(Model modelo) {
+		modelo.addAttribute("usuarios", usuarioService.listarUsuarios());
+		return "listaUsuarios";
+	}
+	
+	@GetMapping("/asignaturas/listar")
+	public String mostrarLista(Model modelo) {
+		modelo.addAttribute("asignaturas", asignaturaService.listarAsignaturas());
+		return "listaAsignaturas";
+	}
+	
+	@ModelAttribute("asignatura")
+	public AsignaturaDTO NuevaAsignatura() {
+		return new AsignaturaDTO();
+	}
+	
+	@GetMapping("/asignaturas/registro")
+	public String mostrarRegistro(Model model) {
+		model.addAttribute("asignatura", new AsignaturaDTO());
+		model.addAttribute("docentes", usuarioService.listarDocentes());
+		return "registroAsignaturas";
+	}
+	
+	@PostMapping("/asignaturas/registro")
+	public String registrarUsuario(@ModelAttribute("asignatura") AsignaturaDTO asignaturaDTO) {
+		if (asignaturaService.validarAsignatura(asignaturaDTO)) {
+			asignaturaService.save(asignaturaDTO);
+			return "redirect:/rector/asignaturas/registro?exito";
+		}else {
+			return "redirect:/rector/asignaturas/registro?error";
+		}
+	}
+	
+	@GetMapping("/asignaturas/modificar")
+	public String mostrarActualizar(Model modelo) {
+		modelo.addAttribute("asignatura", new AsignaturaDTO());
+		modelo.addAttribute("asignaturas", asignaturaService.listarAsignaturas());
+		modelo.addAttribute("docentes", usuarioService.listarDocentes());
+		return "actualizarVehiculo";
+	}
+	
+	@PostMapping("/asignaturas/modificar")
+	public String actualizarAsignatura(@ModelAttribute("asignatura") AsignaturaDTO asignaturaDTO) {
+		if (asignaturaService.validarAsignatura(asignaturaDTO)) {
+			asignaturaService.actualizarUser(asignaturaDTO);
+			return "redirect:/rector/asignaturas/modificar?exito";
+		}else {
+			if(asignaturaService.validarActualizar(asignaturaDTO)) {
+				asignaturaService.actualizarUser(asignaturaDTO);
+				return "redirect:/rector/asignaturas/modificar?exito";
+			}
+		}
+		return "redirect:/rector/asignaturas/modificar?error";
+	}
+	
+	@GetMapping("/asignaturas/docente/listar")
+	public String mostrarDocenteLista(Model modelo, Authentication authentication) {
+		String nombreUsuario = authentication.getName();
+
+	    List<Asignatura> asignaturas = asignaturaService.listarAsignaturas()
+	            .stream()
+	            .filter(a -> a.getDocente() != null &&
+	                         a.getDocente().getNombre().equals(nombreUsuario))
+	            .toList();
+		modelo.addAttribute("asignaturas", asignaturas);
+		return "listaAsignaturas";
+	}
+	
+	@GetMapping("/asignaturas/docente/modificar")
+	public String mostrarDocenteActualizar(Model modelo, Authentication authentication) {
+		String nombreUsuario = authentication.getName();
+
+	    List<Asignatura> asignaturas = asignaturaService.listarAsignaturas()
+	            .stream()
+	            .filter(a -> a.getDocente() != null &&
+	                         a.getDocente().getNombre().equals(nombreUsuario))
+	            .toList();
+		modelo.addAttribute("asignaturas", asignaturas);
+		modelo.addAttribute("asignatura", new AsignaturaDTO());
+		return "actualizarAsigsDocente";
+	}
+	
+	@PostMapping("/asignaturas/docente/modificar")
+	public String actualizarDocenteAsignatura(@ModelAttribute("asignatura") AsignaturaDTO asignaturaDTO) {
+		asignaturaService.actualizarDocente(asignaturaDTO);
+		return "redirect:/rector/asignaturas/modificar?exito";
+	}
+	
+	@GetMapping("/asignaturas/listar/{id}")
+	public String eliminarUsuario(@PathVariable Long id) {
+		asignaturaService.eliminarAsignatura(id);
+		return "redirect:/rector/asignaturas/listar?eliminado";
+	}
+
+}
---
+++ src/main/java/co/edu/local/parcial2/controller/UsuarioController.java
@@ -0,0 +1,67 @@
+package co.edu.local.parcial2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import co.edu.local.parcial2.dto.UsuarioDTO;
+import co.edu.local.parcial2.services.UsuarioService;
+
+@Controller
+public class UsuarioController {
+	
+	@GetMapping("/")
+	public String motrarInicio() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String mostrarLogin() {
+		return "login";
+	}
+	
+	@Autowired
+	private UsuarioService usuarioServicio;
+	
+	@ModelAttribute("usuario")
+	public UsuarioDTO NuevoUsuario() {
+		return new UsuarioDTO();
+	}
+	
+	@GetMapping("/registro")
+	public String mostrarRegistro(Model model) {
+		model.addAttribute("usuario", new UsuarioDTO());
+		return "registro";
+	}
+	
+	@PostMapping("/registro")
+	public String registrarUsuario(@ModelAttribute("usuario") UsuarioDTO usuarioDTO) {
+		if (usuarioServicio.validarUsername(usuarioDTO)) {
+			usuarioServicio.save(usuarioDTO);
+			return "redirect:/registro?exito";
+		}else {
+			return "redirect:/registro?error";
+		}
+	}
+	
+	
+	@GetMapping("/rector/usuarios/registro")
+	public String mostrarRegistrarAdminUsuario(Model modelo) {
+		modelo.addAttribute("usuario", new UsuarioDTO());
+		return "registroAdmin";
+	}
+	
+	@PostMapping("/rector/usuarios/registro")
+	public String registrarAdminUsuario(@ModelAttribute("usuario") UsuarioDTO usuarioDTO) {
+		if (usuarioServicio.validarUsername(usuarioDTO)) {
+			usuarioServicio.saveAdmin(usuarioDTO);
+			return "redirect:/rector/usuarios/registro?exito";
+		}else {
+			return "redirect:/rector/usuarios/registro?error";
+		}
+	}
+
+}
