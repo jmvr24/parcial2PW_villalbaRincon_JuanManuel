ea363f1e14b18ce7738f38da00c44aac8d9c6499
Juan Manuel Villalba Rincon <juanvillalbarincon@gmail.com>
2025-11-04 06:11:24 -0500

    first commit

---
+++ src/main/java/co/edu/local/parcial2/ParcialProgramacionWeb2Application.java
@@ -0,0 +1,13 @@
+package co.edu.local.parcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialProgramacionWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialProgramacionWeb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/local/parcial2/ParcialProgramacionWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.local.parcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialProgramacionWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
